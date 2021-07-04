674ec14c652f11d510d80dd082900ec00b491978
bhaumikmistry <bhaumikpmistry@gmail.com>
2021-07-03 22:59:24 -0400

    add more steps

--- docs/js/script.js
+++ docs/js/script.js
@@ -10,6 +10,8 @@ var step1 = Shape.Prism(Point.ORIGIN,.2,1,.1);
 var step2 = Shape.Prism(Point.ORIGIN,1,.2,.1);
 var dark =new Color(245,205,30);
 var col =new Color(22,190,190);
+var test =new Color(200,100,150);
+
 
 /**
  * Draws an octahedron contained in a 1x1 cube location at origin
@@ -49,28 +51,36 @@ function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   for(var i=0;i<5;i++){
-    iso.add(step1.translate(2+i*.2,0,i*.1+.1),dark); //1
+    iso.add(step1.translate(2+i*.2,0,i*.1+.1),test); //1
   }
+  
   iso.add(Shape.Prism(new Point(4.5,0,.6),1,6,.1),dark); //2
   iso.add(Shape.Prism(new Point(3,0,.6),1.5,1,.1),dark); //1
-//   for(var i=0;i<3;i++){
-//     iso.add(step2.translate(4.5,2+i*.2,.7+i*.1),dark); //2
-//   }
+  
   iso.add(Shape.Prism(new Point(2.6,4.6,1),1.5,1,.1),dark); //3
-//   iso.add(Shape.Prism(new Point(4.5,2.6,1),1,2,.1),dark);//2 
+  
   for(var i=0;i<7;i++){
     iso.add(step1.translate(2.6-i*.2,4.6,1+i*.1),dark); //3
   }
-  iso.add(Shape.Prism(new Point(0.6,5.8,.4),2.2,1,.1),dark); //3
-  iso.add(Shape.Prism(new Point(-0.4,0.4,.4),1,6.4,.1),dark); //4
-//   for(var i=0;i<3;i++){
-//     iso.add(step2.translate(0,2.5+i*.2,i*.1+.1),dark); //4
-//   }
-//   iso.add(Shape.Prism(new Point(0,1,0),1,1.5,.1),dark); //4
+  iso.add(Shape.Prism(new Point(0.6,5.8,.4),2.2,1,.1),test); //3
+  
+  var step5 = Shape.Prism(new Point(0.8,7.8,.4),.2,1,.1);
+  for(var i=25;i>=0;i--){
+    iso.add(step5.translate(2+i*.2,0,i*.1+.1),test); //5
+  }
+
+  var step6 = Shape.Prism(new Point(0.8,9.8,.4),.2,1,.1);
+  for(var i=0;i<5;i++){
+    iso.add(step6.translate(2+i*.2,0,i*.1+.1),test); //6
+  }
+
+  iso.add(Shape.Prism(new Point(0.6,7.8,.4),2.2,1,.1),test); //5
+  iso.add(Shape.Prism(new Point(0.6,9.8,.4),2.2,1,.1),test); //6
+  
+  iso.add(Shape.Prism(new Point(-0.4,0.4,.4),1,10.4,.1),dark); //4
   iso.add(Shape.Prism(new Point(0,0,0),2,1,.1),dark); //1
-
+  
   var p=Shape.Pyramid(Point.ORIGIN);
-
   iso.add(Octahedron(new Point(0,0,0))
     .rotateZ(new Point(.5,.5,.5), angle)
     .scale(new Point(.5,.5,.5),.6,.6,.6),col);
